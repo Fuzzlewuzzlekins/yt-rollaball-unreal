8365ee872326406d25314349b750d476e09ad163
Fuzzlewuzzlekins <47910839+Fuzzlewuzzlekins@users.noreply.github.com>
2025-07-24 18:19:59 +0200

    Add gameplay variables and function signatures to RollaBallPlayer (no functionality yet), remove Tick() and SetupInputPlayerComponent() for now

--- Source/YT_RollaBall_New/Game/RollaBallPlayer.cpp
+++ Source/YT_RollaBall_New/Game/RollaBallPlayer.cpp
@@ -2,12 +2,27 @@
 
 
 #include "RollaBallPlayer.h"
+// YT tutorial: include components
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ARollaBallPlayer::ARollaBallPlayer()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Since the YT tutorial does not use Tick(), we can set this to false. Set to true to restore
+	PrimaryActorTick.bCanEverTick = false;
+	
+	// YT tutorial: create components
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	// YT tutorial: set the Mesh as the Root Component.
+	// Attach the others in a chain, to use each other's transforms.
+	RootComponent = Mesh;
+	SpringArm->SetupAttachment(Mesh); // or SpringArm->SetupAttachment(RootComponent);
+	Camera->SetupAttachment(SpringArm);
 
 }
 
@@ -18,17 +33,29 @@ void ARollaBallPlayer::BeginPlay()
 	
 }
 
-// Called every frame
-void ARollaBallPlayer::Tick(float DeltaTime)
+void ARollaBallPlayer::MoveRight(float Value)
 {
-	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
-void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ARollaBallPlayer::MoveForward(float Value)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
+
+void ARollaBallPlayer::Jump()
+{
+}
+
+// // Called every frame
+// void ARollaBallPlayer::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
+//
+// }
+
+// // Called to bind functionality to input
+// void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+// {
+// 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+//
+// }
 
--- Source/YT_RollaBall_New/Game/RollaBallPlayer.h
+++ Source/YT_RollaBall_New/Game/RollaBallPlayer.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Pawn.h"
 #include "RollaBallPlayer.generated.h"
 
+// YouTube tutorial: forward declarations
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class YT_ROLLABALL_NEW_API ARollaBallPlayer : public APawn
 {
@@ -19,11 +23,41 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	// YouTube tutorial: define components
+	// (Use UPROPERTY to show these in editor but prevent accidental deletion)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UCameraComponent* Camera;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// YouTube tutorial: define physics and behavior variables
+	// (Use UPROPERTY to expose in editor and allow defaults to be tweaked freely)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MoveForce = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float JumpImpulse = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MaxJumpCount = 1;
+	
+public:	
+	// // Called every frame
+	// // YouTube tutorial recommends removing this, will restore if needed
+	// virtual void Tick(float DeltaTime) override;
+
+	// // Called to bind functionality to input
+	// // The YT tutorial keeps this, but I'll comment it out and use a PlayerController instead
+	// virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Custom Pawn action functions (movement)
+	// The YouTube tutorial makes these private, I'll keep them public for use w/ a PlayerController
+	void MoveRight(float Value);
+	void MoveForward(float Value);
+	void Jump();
+	
+private:
+	// YT tutorial: used to track current jump
+	int32 JumpCount = 0;
 
 };
